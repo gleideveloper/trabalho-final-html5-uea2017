082b85b4215ebc2c96377cf950403af805fa59c4
Gleides Vinente <gleidesigner@gmail.com>
2017-05-23 23:01:07 -0400

    update

--- js/game.js
+++ js/game.js
@@ -176,9 +176,7 @@ GameState.prototype.createMapLevel1 = function () {
     this.level1.addTilesetImage('tiles64px', 'mapTiles');
 
     //Cria os layers
-    this.bgLayer = this.level1.createLayer('Bg_0');
-    this.bgLayer = this.level1.createLayer('Bg_1');
-    this.bgLayer = this.level1.createLayer('Bg_2');
+    this.bgLayer = this.level1.createLayer('Bg');
     this.deathLayer = this.level1.createLayer('Death');
     this.superJump = this.level1.createLayer('SuperJump');
     this.trackLayer = this.level1.createLayer('Track');
